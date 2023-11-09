77ff613b69d88a24853a0a2e410c02c8dab05bc3
Vacor <Vacor@DESKTOP-GIBH44K>
2023-11-10 02:56:50 +0300

    Proje dosyası ekle.

---
+++ VB_Hesap_Makinesi/WindowsApplication2/Form1.vb
@@ -0,0 +1,126 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = "0"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "1"
+        Else
+            TextBox1.Text = TextBox1.Text + "1"
+        End If
+
+    End Sub
+
+    Private Sub Button23_Click(sender As Object, e As EventArgs) Handles Button23.Click
+        TextBox1.Text = "0"
+        TextBox1.Focus()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "2"
+        Else
+            TextBox1.Text = TextBox1.Text + "2"
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "3"
+        Else
+            TextBox1.Text = TextBox1.Text + "3"
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "4"
+        Else
+            TextBox1.Text = TextBox1.Text + "4"
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "5"
+        Else
+            TextBox1.Text = TextBox1.Text + "5"
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "6"
+        Else
+            TextBox1.Text = TextBox1.Text + "6"
+        End If
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "0"
+        Else
+            TextBox1.Text = TextBox1.Text + "0"
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "7"
+        Else
+            TextBox1.Text = TextBox1.Text + "7"
+        End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "8"
+        Else
+            TextBox1.Text = TextBox1.Text + "8"
+        End If
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If TextBox1.Text = "0" Then
+            TextBox1.Text = "9"
+        Else
+            TextBox1.Text = TextBox1.Text + "9"
+        End If
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+
+        TextBox1.Text = TextBox1.Text + "+"
+
+
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        Dim hesapla, topla As Integer
+        hesapla = TextBox1.Text
+        If InStr(TextBox1.Text, "+") > 1 Then
+
+        End If
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        TextBox1.Text = TextBox1.Text + "-"
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        TextBox1.Text = TextBox1.Text + "x"
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        TextBox1.Text = TextBox1.Text + "/"
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        TextBox1.Text = TextBox1.Text + ","
+    End Sub
+End Class
